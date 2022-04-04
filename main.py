7f93b788a96ab6443fc2eb066d486c276ae892ae
michaelkendallselwyn <mikekendallselwyn@gmail.com>
2022-04-04 20:56:52 +0100

    Added a recursive fibonacci function that makes use of memoization. Memoization stores computed values so that the function doesn't need to make the repetitive recursive calls.

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ assert fibonacci(5) == 5
 
 print(fibonacci(1))
 print(fibonacci(4))
+
+
+def memoization_fibonacci(position: int, computed: dict = {}) -> int:
+    """This function returns the number at the given position, but uses memoization for efficiency"""
+    if position < 2:
+        return position
+    else:
+        if not computed.get(position):
+            computed[position] = memoization_fibonacci(position - 2, computed) + memoization_fibonacci(position - 1,
+                                                                                                       computed)
+    return computed.get(position)
+
+
+assert memoization_fibonacci(5) == 5
+
+print(memoization_fibonacci(1))
+print(memoization_fibonacci(4))
